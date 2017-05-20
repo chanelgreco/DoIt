67d655b6976b457d968ad7973cc2e156ddad9f96
Chanel Greco <chanel.d.greco@gmail.com>
2017-05-20 21:58:05 +0200

    Initial commit.

---
+++ db/migrate/20170520195417_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[5.0]
+  def change
+    create_table :tasks do |t|
+      t.string :title
+      t.string :description
+      t.integer :priority
+      t.string :status
+      t.date :due_date
+
+      t.timestamps
+    end
+  end
+end
