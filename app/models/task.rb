ac80706ecf46c06ada38cc152f96023c4812410d
Chanel Greco <chanel.d.greco@gmail.com>
2017-05-22 07:44:22 +0200

    The status property of Task now has an enum.

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,16 @@
 class Task < ApplicationRecord
+  validates :title, :description, :priority, :status, presence: true
+  
   enum priority: {
     "Critical" => 0,
     "Important" => 1,
     "Normal" => 2,
     "Low" => 3
+  }
+
+  enum status: {
+    "To do" => 0,
+    "Doing it" => 1,
+    "Done!" => 2
   }
 end
