b389d79fffc2bcf997709c1a2706503f5c90e63a
Chanel Greco <chanel.d.greco@gmail.com>
2017-06-14 15:21:48 +0200

    Fixed an error: the index path was wrong.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to tasks_index, notice: "Logged out"
+    redirect_to tasks_index_path, notice: "Logged out"
   end
 end
