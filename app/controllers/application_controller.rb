45c3fc890e7ed4e165e1126e63abb3b43a91547a
Chanel Greco <chanel.d.greco@gmail.com>
2017-06-14 16:13:37 +0200

    The users now has to log in before a editing or creating new tasks.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :authorize
   protect_from_forgery with: :exception
 
-  private
+  protected
+  # Make sure that a user is logged in before any tasks get displayd
+  def authorize
+    unless User.find_by(id: session[:user_id])
+      redirect_to tasks_index_path, notice: "Please log in"
+    end
+  end
 
+  private
   # Helper to display the current logged in user
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authorize, only: [:index]
 
   # GET /tasks
   # GET /tasks.json
