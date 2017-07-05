dbc0209a3be616dd61e99dffff7d9035be0677e9
chanelgreco <chanel.d.greco@gmail.com>
2017-07-05 16:44:28 +0200

    Changed the seeds so that the user and the tasks are in there respective variables. Also I use the first_or_create instead of the create method.

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,23 +8,19 @@
 
 #users = User.create([{ name: 'Chanel Greco', email: 'chanel.greco@simplificator.com', password_digest: '1234'])
 
-# Destroy all users and tasks when loading this file
-User.destroy_all
-Task.destroy_all
-
-User.create({
+test_user = User.first_or_create!({
   name: "test_user",
   email: "test@test.ch",
   password_digest: BCrypt::Password.create("1234"),
   })
 
-Task.create([{
+tasks = Task.first_or_create!([{
   title: "Testing to do",
   description: "Does this appear in the to do section?",
   priority: 0,
   status: 0,
   due_date: Date.new(2017, 7, 30),
-  user_id: User.first.id
+  user_id: test_user.id
   },
   {
   title: "Testing doing",
@@ -32,7 +28,7 @@ Task.create([{
   priority: 1,
   status: 1,
   due_date: Date.new(2017, 8, 30),
-  user_id: User.first.id
+  user_id: test_user.id
 },
 {
   title: "Testing done",
@@ -40,5 +36,5 @@ Task.create([{
   priority: 2,
   status: 2,
   due_date: Date.new(2017, 8, 15),
-  user_id: User.first.id
+  user_id: test_user.id
 }])
