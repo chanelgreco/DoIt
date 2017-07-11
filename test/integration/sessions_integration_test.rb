cec99028e7af3171aa82ec2f95afe72436714239
chanelgreco <chanel.d.greco@gmail.com>
2017-07-11 16:06:04 +0200

    Added the integration tests for the sessions controller.

---
+++ test/integration/sessions_integration_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = users(:user)
+  end
+
+  test "should get new" do
+    get login_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    post login_url
+    # log in
+    sign_in_as(@user)
+    assert_equal "Login successfull", flash[:notice]
+  end
+
+  test "should get destroy" do
+    delete logout_url
+    assert_equal "Logged out", flash[:notice]
+    assert_redirected_to tasks_index_url
+  end
+
+end
