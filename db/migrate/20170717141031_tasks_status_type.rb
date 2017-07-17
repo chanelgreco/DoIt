5c9e3823581feb87f574610a8bf935218a62102e
chanelgreco <chanel.d.greco@gmail.com>
2017-07-17 16:52:19 +0200

    Change the migration file for the postgresql bug.

--- db/migrate/20170717141031_tasks_status_type.rb
+++ db/migrate/20170717141031_tasks_status_type.rb
@@ -1,5 +1,5 @@
 class TasksStatusType < ActiveRecord::Migration[5.0]
   def change
-    change_column :tasks, :status, "integer USING CAST(status AS integer)"
+    change_column :tasks, :status, "integer using status::integer"
   end
 end
