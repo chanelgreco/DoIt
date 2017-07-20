288b74a82fbd3556a83799ce2db4899e49b1f45b
chanelgreco <chanel.d.greco@gmail.com>
2017-07-20 14:21:58 +0200

    Refactored the scopes of the tasks model.

--- app/models/task.rb
+++ app/models/task.rb
@@ -15,22 +15,24 @@ class Task < ApplicationRecord
 
   enum status: {
     "To do" => STATUS_TODO,
-    "Doing it..." => 1,
-    "Done!" => 2
+    "Doing it..." => STATUS_DOING,
+    "Done!" => STATUS_DONE
   }
 
 
   # Scope to show the tasks filtered by their status.
   scope :todo, -> { where(status: STATUS_TODO).order(:due_date) }
-  scope :doing, -> { where(status: 1).order(:due_date) }
-  scope :done, -> { where(status: 2).order(:due_date) }
+  scope :doing, -> { where(status: STATUS_DOING).order(:due_date) }
+  scope :done, -> { where(status: STATUS_DONE).order(:due_date) }
 
+  # Scope to show the tasks with the status todo and Doing
+  scope :todo_doing, -> { where(status: [STATUS_TODO, STATUS_DOING])}
 
   # Scope to show overdue tasks with the status "To do" or "Doing it..."
-  scope :overdue, -> { where(status: [0, 1]).where("due_date <= ?", Date.today ).order(:due_date) }
+  scope :overdue, -> { todo_doing.where("due_date <= ?", Date.today ).order(:due_date) }
 
   # Scope to show upcoming tasks with the status "To do" or "Doing it..."
-  scope :upcoming, -> { where(status: [0, 1]).where(due_date: (Date.current)..Date.current+7).order(:due_date).limit(6) }
+  scope :upcoming, -> { todo_doing.where(due_date: (Date.current)..Date.current+7).order(:due_date).limit(6) }
 
   # Method to check if task is overdue
   def overdue?
