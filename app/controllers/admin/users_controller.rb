d43b9529681298203eb07f7c6a94914e89189889
chanelgreco <chanel.d.greco@gmail.com>
2017-07-24 14:12:09 +0200

    Admin can set the admin flag for other users.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -65,6 +65,6 @@ class Admin::UsersController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
   end
 end
